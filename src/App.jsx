eca6ce90cf1d8aec5d3f932efb06369cbedf5323
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-07-16 18:26:00 -0300

    Modificando el Launch List y agregando el Launch Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import {  Image } from '@chakra-ui/react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import logo from './assets/logo.png'
-import { LaunchItem } from './components/LaunchItem';
-
-
+import { LaunchList } from './components/LaunchList';
+import {LaunchDetails} from './components/LaunchDetails'
+ 
 /*
 En el ciclo de vida del componente primero se ejecuta el useEffect, va a llamar a la API, va a setear el estado, y cuando
 launches ya tenga algo, va a pintar el html
@@ -14,8 +14,8 @@ export function App() {
    <>
    <Image m = {4} p = {4} src={logo} width={300}/>
    <Routes>
-      <Route path="/" element={<LaunchesList/>} />
-      <Route path="launch/:id" element={<LaunchDetail/>}/>
+      <Route path="/" element={<LaunchList/>} />
+      <Route path="launch/:launchid" element={<LaunchDetails/>}/>
    </Routes>
 
    </>
--- src/components/LaunchList.jsx
+++ src/components/LaunchList.jsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
-import { Heading, Image } from '@chakra-ui/react';
-import * as API from './services/launches';
-//El use state devuelve un array con el estado en si, y el set es una función que permite cambiar ese estado
-const [launches, setLaunches] = useState([]);
+import { Heading } from '@chakra-ui/react';
+import { LaunchItem } from './LaunchItem';
+import * as API from '../services/launches';
 
-//El use effect el argumento que recibe es una arrow function
+export function LaunchList(){
+    //El use state devuelve un array con el estado en si, y el set es una función que permite cambiar ese estado
+    const [launches, setLaunches] = useState([]);
 
-useEffect(() => {
-    //cuando acaba la promesa vamos a obtener los datos y se los pasamos al estado
-    API.getAllLaunches().then(setLaunches);
-    //así como recibe una arrow function de primer argumento, el segundo es un array de dependencias
-    //Si el array está vacío, el use effect se va a ejecutar cuando se crea el componente
-    //si en el array ponemos otra dependencia, llama a la funcion cada vez que se actualice la variable
-},[]);
+    //El use effect el argumento que recibe es una arrow function
+
+    useEffect(() => {
+        //cuando acaba la promesa vamos a obtener los datos y se los pasamos al estado
+        API.getAllLaunches().then(setLaunches);
+        //así como recibe una arrow function de primer argumento, el segundo es un array de dependencias
+        //Si el array está vacío, el use effect se va a ejecutar cuando se crea el componente
+        //si en el array ponemos otra dependencia, llama a la funcion cada vez que se actualice la variable
+    },[]);
 
 
-//El código js dentro de jsx se mete por medio de llaves
-/*recorremos el array y por cada elemento decimos que nos devuelva elementos html, por ejemplo en este
-caso un li, es importante ponerle un atributo key al listado, para que el valor de key sea único por 
-cada elemento*/ 
-export function LaunchesList(){
+    //El código js dentro de jsx se mete por medio de llaves
+    /*recorremos el array y por cada elemento decimos que nos devuelva elementos html, por ejemplo en este
+    caso un li, es importante ponerle un atributo key al listado, para que el valor de key sea único por 
+    cada elemento*/ 
     return(
         <>
         <Heading as= "h1" size = "lg">
