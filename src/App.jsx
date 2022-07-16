299b6dfd834bd42b44fedbe62cb3e341167a1250
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-07-16 17:47:23 -0300

    Creando componente LaunchList y definiendo su función

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react';
-import { Heading, Image } from '@chakra-ui/react';
+import {  Image } from '@chakra-ui/react';
 import { Routes, Route, Link } from "react-router-dom";
 import logo from './assets/logo.png'
-import * as API from './services/launches';
 import { LaunchItem } from './components/LaunchItem';
 
 
@@ -11,42 +9,17 @@ En el ciclo de vida del componente primero se ejecuta el useEffect, va a llamar 
 launches ya tenga algo, va a pintar el html
 */ 
 export function App() { 
-   //El use state devuelve un array con el estado en si, y el set es una función que permite cambiar ese estado
-  const [launches, setLaunches] = useState([]);
 
-  //El use effect el argumento que recibe es una arrow function
-
-  useEffect(() => {
-    //cuando acaba la promesa vamos a obtener los datos y se los pasamos al estado
-    API.getAllLaunches().then(setLaunches);
-    //así como recibe una arrow function de primer argumento, el segundo es un array de dependencias
-    //Si el array está vacío, el use effect se va a ejecutar cuando se crea el componente
-    //si en el array ponemos otra dependencia, llama a la funcion cada vez que se actualice la variable
-  },[]);
-
-
-  //El código js dentro de jsx se mete por medio de llaves
-  /*recorremos el array y por cada elemento decimos que nos devuelva elementos html, por ejemplo en este
-  caso un li, es importante ponerle un atributo key al listado, para que el valor de key sea único por 
-  cada elemento*/ 
   return (
    <>
    <Image m = {4} p = {4} src={logo} width={300}/>
    <Routes>
       <Route path="/" element={<LaunchesList/>} />
-      <Route path="launch/:id" element={<LaunchItem/>}/>
+      <Route path="launch/:id" element={<LaunchDetail/>}/>
    </Routes>
-    <Heading as= "h1" size = "lg">
-      SpaceX Launches
-    </Heading>
-    <section>
-    
-      {launches.map((launch) =>( 
-        <LaunchItem key= {launch.flight_number}{...launch}/>
-      ))}
-    </section>
+
    </>
-  )
+  );
 }
 
 
